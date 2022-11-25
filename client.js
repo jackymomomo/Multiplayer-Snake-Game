8c18eb311c3b2cdc84271b18fabd5c39c61c680a
jackymomomo <jacklloydrobinson@gmail.com>
2022-11-24 19:52:12 -0800

    write Name: ___ on snake if connection passes

--- client.js
+++ client.js
@@ -1,24 +1,33 @@
 const net = require("net");
 
-// establishes a connection with the game server
+
+// establishes a clientection with the game server
 const connect = function () {
   const conn = net.createConnection({
     host: "localhost",
     port: 50541
   });
-
-  // update connect function to handle data 
-conn.on('data', (message) => {
-console.log(message)
-} )
-
   // interpret incoming data as text
   conn.setEncoding("utf8");
+
+// add clientect event 
+conn.on('connect', () => {
+ conn.write('Name: JLR')
+ console.log('connected god')
+});
+  // update clientect function to handle data 
+  conn.on("data", message => {
+    console.log(message);
+  });
+
+conn.on('end', () => {
+console.log('conn is disconnect')
+})
 
   return conn;
 };
 
-console.log("Connecting ...");
+console.log("connecting ...");
 connect();
 
 module.export = connect;
