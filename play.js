d85b4793947dc029e23ff149a8b3b6fb2bb0dfa5
jackymomomo <jacklloydrobinson@gmail.com>
2022-11-24 16:27:39 -0800

    added client side server and exported it to the play js file

--- play.js
+++ play.js
@@ -1,22 +1,2 @@
-const net = require("net");
+const connect = require('./client')
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
-  });
-
-  // update connect function to handle data 
-conn.on('data', (message) => {
-console.log(message)
-} )
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
